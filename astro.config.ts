25e9eb170939c929251f908fe690c4c02994cbf7
Ryan Valor <272567571@qq.com>
2026-05-25 14:00:03 +0800

    chore(config): add build.assetsPrefix

--- astro.config.ts
+++ astro.config.ts
@@ -20,6 +20,9 @@ import { rehypeMermaid } from "./src/utils/rehype/beautiful-mermaid";
 
 export default defineConfig({
   site: config.site.url,
+  // build:{
+  //   assetsPrefix:
+  // },
   integrations: [
     mdx(),
     sitemap({
